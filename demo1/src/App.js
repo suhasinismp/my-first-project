2df9e4ab49fc2062e8df40d1fbf76327a0f76397
suhasini <suhasinismp@gmail.com>
2024-01-31 16:32:20 +0530

    change in message

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        Welcome to React Application
+        Welcome to Reactnative application
         </a>
       </header>
     </div>
